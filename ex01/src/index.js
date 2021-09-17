1f240ac3f4aa4c54e5b92464db0facf7b2c751bc
abidmujkic <abidmujkic1@gmail.com>
2021-09-17 18:33:45 +0200

    ex01

---
+++ ex01/src/index.js
@@ -0,0 +1,21 @@
+function main() {
+    
+    var myFirstString;
+    var mySecondString;
+    
+    // Only change code below this line
+
+    myFirstString = "I think therefore I \"am\" -- sentient."
+    mySecondString = "No, you only \"think\", so you aren't."
+
+    // Only Change code above this line
+    
+    return {
+        myFirstString,
+        mySecondString
+    };
+    
+
+}
+console.log(main());
+module.exports = main;
