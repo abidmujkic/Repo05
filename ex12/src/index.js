9f89a0d86ab29cfacb0ae699cd541cb76d35cc38
abidmujkic <abidmujkic1@gmail.com>
2021-09-18 08:24:36 +0200

    ex12

---
+++ ex12/src/index.js
@@ -0,0 +1,16 @@
+function main() {
+
+    //Only change code below this line
+
+    var lastLetterOfLastName;
+    var lastName = "Tesla"; 
+    lastLetterOfLastName = lastName[lastName.length-1];
+    
+    //Only change code above this line
+
+    
+    return lastLetterOfLastName;
+
+}
+console.log(main());
+module.exports = main;
